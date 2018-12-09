df28688dde23fe5f20d0291f2d31b0c13addcf25
github username <github email address>
2018-12-08 21:20:12 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,39 +1,16 @@
-# class Application
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#
-#     num_1 = Kernel.rand(1..20)
-#     num_2 = Kernel.rand(1..20)
-#     num_3 = Kernel.rand(1..20)
-#
-#     resp.write "#{num_1}\n"
-#     resp.write "#{num_2}\n"
-#     resp.write "#{num_3}\n"
-#
-#     if num_1==num_2 && num_2==num_3
-#       resp.write "You Win"
-#     else
-#       resp.write "You Lose"
-#     end
-#
-#     resp.finish
-#   end
-#
-# end
-
-
-#If it's before noon, greet the user with "Good Morning!"
-#If it's after noon, greet the user with "Good Afternoon!". Remember that getting the time may return results in 24 hour time! Anytime after 12 is the afternoon.
-
 class Application
+
   def call(env)
     resp = Rack::Response.new
 
-    time = time.now
-    resp.write "time #{time}"
+    time = Time.now
 
-
-
+    if time.hour >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
   end
+
 end
